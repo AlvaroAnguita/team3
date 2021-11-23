b6b8b54e4a75afcc162857ef75253b0c31001710
patricia <patricia.poveda-boto@capgemini.com>
2021-11-23 11:35:30 +0100

    add clienteService

--- src/main/java/com/prueba/service/ClienteService.java
+++ src/main/java/com/prueba/service/ClienteService.java
@@ -1,5 +1,15 @@
 package com.prueba.service;
 
-public class ClienteService {
+import java.util.List;
+
+import com.prueba.models.entity.Cliente;
+
+
+
+public interface ClienteService {
+public List<Cliente> findAll();
+public Cliente findById(Long id);
+public Cliente save(Cliente cliente);
+public void delete(Long id);
 
 }
