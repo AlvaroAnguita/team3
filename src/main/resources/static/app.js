a7e2334ac9acb9b902e28ef2a9f8f02d67d9dacd
patricia <patricia.poveda-boto@capgemini.com>
2021-11-24 09:29:21 +0100

    crear usuario

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -32,7 +32,7 @@ const renderAlumnos = () => {
 		codigoP.innerHTML = iterator.codigoP;
 		imagen.innerHTML = iterator.imagen;
 		dni.innerHTML = iterator.dni;
-		
+
 	};
 }
 
@@ -51,3 +51,44 @@ const borrarAlumno = () => {
 }
 borrarAlum.addEventListener("click", borrarAlumno);
 
+
+const btnSubmitCrear = document.querySelector(".js-formCreate");
+
+const crearAlumno = (ev) => {
+	ev.preventDefault();
+	const nombre = document.querySelector(".js-nombreC").value;
+	const apellidos = document.querySelector(".js-apellidosC").value;
+	const email = document.querySelector(".js-emailC").value;
+	const telefono = document.querySelector(".js-telefonoC").value;
+	const direccion = document.querySelector(".js-direccionC").value;
+	const codigoP = document.querySelector(".js-cpC").value;
+	const imagen = document.querySelector(".js-imagenC").value;
+	const dni = document.querySelector(".js-dniC").value;
+
+	const alumno = {
+		nombre: nombre,
+		apellido: apellidos,
+		email: email,
+		telefono: telefono,
+		direccion: direccion,
+		codigoP: codigoP,
+		imagen: imagen,
+		dni: dni
+	}
+
+	fetch('http://localhost:8080/team3/clientes', {
+		method: "POST",
+		body: JSON.stringify(alumno),
+		headers: { "Content-Type": "application/json" }
+	})
+
+		.then((res) => res.json())
+		.then((data) => {
+			data = data;
+			console.log(data.mensaje);
+
+		});
+
+
+}
+btnSubmitCrear.addEventListener("submit", crearAlumno);
